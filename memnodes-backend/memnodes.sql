eae3b38df8207e471b82cf968b0984a2b009eaba
turtlebeasts <179turtleshells@gmail.com>
2025-08-04 20:32:32 +0530

    database exported

--- memnodes-backend/memnodes.sql
+++ memnodes-backend/memnodes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2025 at 07:37 PM
+-- Generation Time: Aug 04, 2025 at 05:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,8 +34,20 @@ CREATE TABLE `events` (
   `description` text DEFAULT NULL,
   `date` date DEFAULT NULL,
   `next_event_id` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `position` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `timeline_id`, `title`, `description`, `date`, `next_event_id`, `created_at`, `position`) VALUES
+(15, 29, 'Went to school', 'Today I went to school and had fun', '2025-01-01', 17, '2025-08-02 15:30:28', 0),
+(16, 29, 'went to new school', 'went to new school but this time it was not that great', '2025-01-01', 18, '2025-08-02 15:30:52', 3),
+(17, 29, 'a', 'aaaaa', '2025-01-01', 19, '2025-08-02 15:31:03', 1),
+(18, 29, 'b', 'b', '2025-01-01', NULL, '2025-08-02 15:31:10', 4),
+(19, 29, 'c', 'ccccc', '2025-01-01', 16, '2025-08-02 15:31:23', 2);
 
 -- --------------------------------------------------------
 
@@ -51,6 +63,13 @@ CREATE TABLE `timelines` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `timelines`
+--
+
+INSERT INTO `timelines` (`id`, `user_id`, `title`, `is_public`, `created_at`) VALUES
+(29, 2, 'school time', 1, '2025-08-02 07:36:30');
+
 -- --------------------------------------------------------
 
 --
@@ -65,6 +84,14 @@ CREATE TABLE `users` (
   `bio` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `bio`, `created_at`) VALUES
+(2, 'Mriganka', 'mrigz179@gmail.com', '$2b$10$ZxBQ9D4BjjhFHO4HxlAtJ.wGTMfeGxT/0vpD0dtbyMzeKRJKxmhdK', NULL, '2025-07-25 16:46:55'),
+(4, 'Ankita', 'ankita@gmail.com', '$2b$10$We5RK9KqxRyrQgVft5bqdOwUKHdJTuEdNynQ92fsXw8P3siOmYmka', NULL, '2025-07-25 19:34:04');
 
 --
 -- Indexes for dumped tables
@@ -100,19 +127,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `timelines`
 --
 ALTER TABLE `timelines`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
